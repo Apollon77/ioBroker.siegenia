3f68f544f86f40cbc5e3826ab4a6f70f460d1bc9
Ingo Fischer <github@fischer-ka.de>
2019-01-27 19:52:20 +0100

    initial commit, nothing works beside lib and test

---
+++ admin/words.js
@@ -0,0 +1,38 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'siegenia adapter settings': {
+        'en': 'Adapter settings for siegenia',
+        'de': 'Adaptereinstellungen für siegenia',
+        'ru': 'Настройки адаптера для siegenia',
+        'pt': 'Configurações do adaptador para siegenia',
+        'nl': 'Adapterinstellingen voor siegenia',
+        'fr': "Paramètres d'adaptateur pour siegenia",
+        'it': "Impostazioni dell'adattatore per siegenia",
+        'es': 'Ajustes del adaptador para siegenia',
+        'pl': 'Ustawienia adaptera dla siegenia'
+    },
+    'option 1 description': {
+        'en': 'Option 1 is cool',
+        'de': 'Option 1 ist cool',
+        'ru': 'Вариант 1 - это круто',
+        'pt': 'Opção 1 é legal',
+        'nl': 'Optie 1 is cool',
+        'fr': "L'option 1 est cool",
+        'it': "L'opzione 1 è cool",
+        'es': 'La opción 1 es genial',
+        'pl': 'Opcja 1 jest fajna'
+    },
+    'option 2 description': {
+        'en': 'Option 2 is not',
+        'de': 'Option 2 nicht',
+        'ru': 'Вариант 2 не',
+        'pt': 'A opção 2 não é',
+        'nl': 'Optie 2 is dat niet',
+        'fr': "L'option 2 n'est pas",
+        'it': "L'opzione 2 no",
+        'es': 'La opción 2 no es',
+        'pl': 'Opcja 2 nie jest'
+    }
+};
